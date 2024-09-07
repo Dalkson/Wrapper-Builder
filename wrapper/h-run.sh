3ffdfb59003f49dd0b147e9e6bcf9b709882fdd0
Dalkson <26678089+Dalkson@users.noreply.github.com>
2024-09-07 14:51:46 -0400

    Update h-run.sh

--- wrapper/h-run.sh
+++ wrapper/h-run.sh
@@ -35,7 +35,5 @@ CLEAN=$(echo "$CUSTOM_USER_CONFIG" | sed -E 's/-arch [^ ]+ //')
 echo "args are now: $CLEAN"
 echo "We are using miner: $MINER"
 echo $(date +%s) > "/tmp/miner_start_time"
-/hive/miners/custom/$MINER/$MINER -v 2>&1 | grep 'Miner version:' | awk '{print $3}' > /tmp/.tnn-miner-version
-/hive/miners/custom/$MINER/$MINER $CLEAN --broadcast 2>&1 | tee -a  ${CUSTOM_LOG_BASENAME}.log
 echo "Miner has exited"
 
